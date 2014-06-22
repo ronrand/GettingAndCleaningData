2a902810617b455cb3cfb80730e0a1cf20e5ebb3
ronrand <randolph.ron@gmail.com>
2014-06-22 17:29:05 -0500

    Script Update
    Revision

--- run_analysis.R
+++ run_analysis.R
@@ -19,12 +19,12 @@ subJoin <- rbind(subTrain, subTest)
 
 ## Extract only the measurements on the mean and standard deviation of each measurement.
 features <- read.table("./UCI HAR Dataset/features.txt")
-meanStdMeasure <- grep("mean\\(\\)|std\\(\\)", features[, 2])
-dataJoin <- dataJoin[, meanStdMeasure]
+meanStdMeasurement <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+dataJoin <- dataJoin[, meanStdMeasurement]
 
 
 ## Use descriptive activity names to name the activities in the dataset
-colnames(dataJoin) <- gsub("\\(\\)", "", features[meanStdMeasure, 2]) # remove parentheses
+colnames(dataJoin) <- gsub("\\(\\)", "", features[meanStdMeasurement, 2]) # remove parentheses
 colnames(dataJoin) <- gsub("mean", "Mean", colnames(dataJoin)) # capitalize M
 colnames(dataJoin) <- gsub("std", "Std", colnames(dataJoin)) # capitalize S
 colnames(dataJoin) <- gsub("-", "", colnames(dataJoin)) # remove dash in column names 
@@ -42,7 +42,7 @@ substr(activity[4, 2], 1, 1) <- toupper(substr(activity[4, 2], 1, 1))
 substr(activity[5, 2], 1, 1) <- toupper(substr(activity[5, 2], 1, 1))
 substr(activity[6, 2], 1, 1) <- toupper(substr(activity[6, 2], 1, 1))
 
-# Merge Data
+# Label Data
 activityLabel <- activity[labelJoin[, 1], 2]
 labelJoin[, 1] <- activityLabel
 
@@ -52,31 +52,31 @@ colnames(labelJoin) <- "activity"
 colnames(subJoin) <- "subject"
 
 # Merge Data
-cleanedData <- cbind(subJoin, labelJoin, dataJoin)
+cleaned <- cbind(subJoin, labelJoin, dataJoin)
 
 
 # write out the 1st dataset as tab delimited text file
-write.table(cleanedData, "TidyDataSet.txt", sep='\t', quote=FALSE, row.names=FALSE) 
+write.table(cleaned, "TidyDataSet.txt", sep='\t', quote=FALSE, row.names=FALSE) 
 
 ## Create a second, independent tidy dataset with the average of each variable for each activity and each subject. 
 
 subLength <- length(table(subJoin)) # 30
 activityLen <- dim(activity)[1] # 6
-columnLen <- dim(cleanedData)[2] #68
-result <- matrix(NA, nrow=subLength*activityLen, ncol=columnLen) 
+columnLength <- dim(cleaned)[2] #68
+result <- matrix(NA, nrow=subLength*activityLen, ncol=columnLength) 
 result <- as.data.frame(result)
-colnames(result) <- colnames(cleanedData)
+colnames(result) <- colnames(cleaned)
+dim(result)
 
-
-
+## calculate mean
 row <- 1
 for(i in 1:subLength) {
     for(j in 1:activityLen) {
         result[row, 1] <- sort(unique(subJoin)[, 1])[i]
         result[row, 2] <- activity[j, 2]
-        bool1 <- i == cleanedData$subject
-        bool2 <- activity[j, 2] == cleanedData$activity
-        result[row, 3:columnLen] <- colMeans(cleanedData[bool1&bool2, 3:columnLen])
+        mtx1 <- i == cleaned$subject
+        mtx2 <- activity[j, 2] == cleaned$activity
+        result[row, 3:columnLength] <- colMeans(cleaned[mtx1&mtx2, 3:columnLength])
         row <- row + 1
     }
 }
